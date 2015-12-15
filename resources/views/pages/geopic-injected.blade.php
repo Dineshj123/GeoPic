498cc5c79b8cf2159b520efe34e1e3740b80d6c7
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2015-12-15 17:19:54 +0530

    add stylesheet.css

--- resources/views/pages/geopic-injected.blade.php
+++ resources/views/pages/geopic-injected.blade.php
@@ -3,16 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Geopic</title>
+
+      <link rel="stylesheet" href="/public/stylesheet.css" media="screen" title="no title" charset="utf-8">
+
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-     <style>
-  .carousel-inner > .item > img,
-  .carousel-inner > .item > a > img {
-      width: 50%;
-      margin: auto;
-  }
-  </style>
+
   </head>
   <body>
     <div class="container">
--- resources/views/pages/geopic.blade.php
+++ resources/views/pages/geopic.blade.php
@@ -2,44 +2,56 @@
 <!DOCTYPE html>
 <html>
   <head>
+
     <meta charset="utf-8">
     <title>Geopic</title>
 
       <!--Include Bootstrap... -->
+     <link rel="stylesheet" href="/stylesheet.css" media="screen" title="no title" charset="utf-8">
      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
   </head>
   <body>
-  <div class="row">
-      <div class="container">
-        <div class="jumbotron">
-          <h1>Geopic</h1>
+
+      <div class="row">
+        <div class="container">
+          <div class="jumbotron" id="title">
+            <h1>Geopic</h1>
+          </div>
         </div>
       </div>
-  </div>
 
-  <div class="row">
-    <div class="col-sm-3">
+      <div class="row" >
 
-    </div>
-    <div class="col-sm-7">
-      <form class="form-horizontal" role="form" action="/search" method="POST">
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="location">Location:</label>
-            <div class="col-sm-5">
-              <input type="text" class="form-control" id="location" placeholder="Enter loction" name="location">
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-5">
-              <button type="submit" class="btn btn-default">Submit</button>
-            </div>
-          </div>
-      </form>
-    </div>
+        <div class="col-sm-3">
+        </div>
 
-  </div>
+        <div class="col-sm-7" id="body">
+          <form class="form-horizontal" role="form" action="/search" method="POST">
+              <div class="form-group">
+                <label class="control-label col-sm-2" for="location">Location:</label>
+                <div class="col-sm-5">
+                  <input type="text" class="form-control" id="location" placeholder="Enter loction" name="location">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-5">
+                  <button type="submit" class="btn btn-default">Submit</button>
+                </div>
+              </div>
+          </form>
+        </div>
+
+      </div>
+      <div class="row" >
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8" id="info">
+          <b>Geopic</b> <br>
+          Find photos from your favourite locations
+        </div>
+        <div class="col-sm-2"></div>
+      </div>
   </body>
 </html>
