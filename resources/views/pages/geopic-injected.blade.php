f98adfe805e01c3cda79d8d8f1a2263d8d25b64d
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2015-12-16 18:30:37 +0530

    update stylesheet.css

--- resources/views/pages/geopic-injected.blade.php
+++ resources/views/pages/geopic-injected.blade.php
@@ -12,71 +12,72 @@
 
   </head>
   <body>
+    <div class="row" id="content">
 
-    <div class="row" id="header">
-      <div class="container">
-        <div class="jumbotron" id="title">
-          <h1>Geopic</h1>
+      <div class="row" id="header">
+        <div class="container">
+          <div class="jumbotron" id="title">
+            <h1>Geopic</h1>
+          </div>
         </div>
       </div>
-    </div>
-    <br><br><br>
-    <div class="row">
-          <div class="col-sm-3">
+      <br><br><br>
+      <div class="row">
+            <div class="col-sm-3">
+            </div>
+            <div class="col-sm-6">
+              <div id="geocarousel" class="carousel slide" data-ride="carousel">
+                  <!-- Indicators -->
+                  <ol class="carousel-indicators">
+                    @for($i=0;$i<$size;$i++)
+                      @if($i==0)
+                        <li data-target="#geocarousal" data-slide-to="0" class="active"></li>
+                      @else
+                        <li data-target="#geocarousal" data-slide-to={{$i}}></li>
+                      @endif
+                    @endfor
+                  </ol>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              @for($i=0;$i<$size;$i++)
+                @if($i == 0)
+                  <div class="item active">
+                @else
+                  <div class="item">
+                @endif
+                  <img src="{{$instagram_array['data'][$i]['images']['low_resolution']['url']}}" alt="{{$instagram_array['data'][$i]['location']['name']}}" />
+                </div>
+              @endfor
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#geocarousel" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+
+            <a class="right carousel-control" href="#geocarousel" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+
           </div>
-          <div class="col-sm-6">
-            <div id="geocarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                  @for($i=0;$i<$size;$i++)
-                    @if($i==0)
-                      <li data-target="#geocarousal" data-slide-to="0" class="active"></li>
-                    @else
-                      <li data-target="#geocarousal" data-slide-to={{$i}}></li>
-                    @endif
-                  @endfor
-                </ol>
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner" role="listbox">
-            @for($i=0;$i<$size;$i++)
-              @if($i == 0)
-                <div class="item active">
-              @else
-                <div class="item">
-              @endif
-                <img src="{{$instagram_array['data'][$i]['images']['low_resolution']['url']}}" alt="{{$instagram_array['data'][$i]['location']['name']}}" />
-              </div>
-            @endfor
-
-          <!-- Left and right controls -->
-          <a class="left carousel-control" href="#geocarousel" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-
-          <a class="right carousel-control" href="#geocarousel" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-
-        </div>
+      </div>
+    </div>
+    <div class="col-sm-3">
     </div>
   </div>
-  <div class="col-sm-3">
+  <br>
+  <div class="row">
+    <div class="col-sm-2">
+    </div>
+    <div class="col-sm-8">
+      <a href="/geopic">
+        <button type="button" name="button" class="btn btn-info btn-block">search</button>
+      </a>
+    </div>
+    <div class="col-sm-2">
+    </div>
   </div>
 </div>
-<br>
-<div class="row">
-  <div class="col-sm-2">
-  </div>
-  <div class="col-sm-8">
-    <a href="/geopic">
-      <button type="button" name="button" class="btn btn-info btn-block">search</button>
-    </a>
-  </div>
-  <div class="col-sm-2">
-  </div>
-</div>
-
   </body>
 </html>
--- resources/views/pages/geopic.blade.php
+++ resources/views/pages/geopic.blade.php
@@ -15,56 +15,58 @@
 
   </head>
   <body>
+    <div class="row" id="content">
 
-      <div class="row" id="header">
-        <div class="container">
-          <div class="jumbotron" id="title">
-            <h1>Geopic</h1>
+  
+        <div class="row" id="header">
+          <div class="container">
+            <div class="jumbotron" id="title">
+              <h1>Geopic</h1>
+            </div>
           </div>
         </div>
-      </div>
 
-      <br><br><br>
+        <br><br><br>
 
-      <div class="row" >
+        <div class="row" >
 
-          <div class="col-sm-3">
+            <div class="col-sm-3">
+            </div>
+
+            <div class="col-sm-7" id="body">
+              <form class="form-horizontal" role="form" action="/search" method="POST">
+                  <div class="form-group">
+                    <label class="control-label col-sm-2" for="location">Location:</label>
+                    <div class="col-sm-5">
+                      <input type="text" class="form-control" id="location" placeholder="Enter location" name="location">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-5">
+                      <button type="submit" class="btn btn-default">Submit</button>
+                    </div>
+                  </div>
+              </form>
+            </div>
+        </div>
+
+        <br><br><br><br><br><br>
+
+        <div class="row" id="footer">
+
+          <div class="col-sm-2"></div>
+
+          <div class="col-sm-8" id="info" >
+            <br>
+              Find photos from your favourite locations...
+            <br><br>
           </div>
 
-          <div class="col-sm-7" id="body">
-            <form class="form-horizontal" role="form" action="/search" method="POST">
-                <div class="form-group">
-                  <label class="control-label col-sm-2" for="location">Location:</label>
-                  <div class="col-sm-5">
-                    <input type="text" class="form-control" id="location" placeholder="Enter location" name="location">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="col-sm-offset-2 col-sm-5">
-                    <button type="submit" class="btn btn-default">Submit</button>
-                  </div>
-                </div>
-            </form>
-          </div>
-      </div>
+          <div class="col-sm-2"></div>
 
-      <br><br><br><br><br><br>
-
-      <div class="row" id="footer">
-
-        <div class="col-sm-2"></div>
-
-        <div class="col-sm-8" id="info" >
-          <br>
-            Find photos from your favourite locations...
           <br><br>
+
         </div>
-
-        <div class="col-sm-2"></div>
-
-        <br><br>
-
       </div>
-
   </body>
 </html>
